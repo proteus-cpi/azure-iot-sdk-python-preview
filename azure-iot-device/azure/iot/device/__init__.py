de74ab5f4a4f5f45ba42eed2c40e2f1d6d5f0ab6
Carter Tinney <catinney@microsoft.com>
2019-04-12 12:33:00 -0700

    Restructred namespace

---
+++ azure-iot-device/azure/iot/device/__init__.py
@@ -0,0 +1,7 @@
+""" Azure IoT Device Library
+
+This library provides clients and associated models for communicating with Azure IoT services
+from an IoT device.
+"""
+
+from .iothub import *
---
+++ azure-iot-device/azure/iot/device/common/__init__.py
@@ -0,0 +1,5 @@
+"""Azure IoT Device Common
+
+This package provides shared modules for use with various Azure IoT device-side clients.
+INTERNAL USAGE ONLY
+"""
